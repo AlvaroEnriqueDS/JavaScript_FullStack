6dcdd05bb5139b4bffed492948396a12f719cc2c
AlvaroEnriqueDS <alvaroenrique020@gmail.com>
2019-09-04 16:35:18 -0500

    vue js corriendo v1

---
+++ vue-js/08_platzi_music/prueba_vue/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'pruebavue',
+    apikey: '2839aa7bc52ca7fe9843d94dbb33e77c',
+    secret: '3df8742698fffdc81e0e740152ba24fa',
+    registeredTo: 'alvaroenriqueds'
+}
+
+// Application name	pruebavue
+// API key	2839aa7bc52ca7fe9843d94dbb33e77c
+// Shared secret	3df8742698fffdc81e0e740152ba24fa
+// Registered to	alvaroenriqueds
